4500869a219505638291451c5f9df49c7b632961
singhankita29 <100141535+singhankita29@users.noreply.github.com>
2022-09-29 01:27:22 +0530

    day_2

---
+++ Frontend/src/Homepage/MainHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+const MainHome = () => {
+  return (
+    <div>
+        <Navbar/>
+
+        <Footer />
+    </div>
+  )
+}
+
+export default MainHome
