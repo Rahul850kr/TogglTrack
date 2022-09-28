cca0982b95ffb575d0558783993c2acd0d46e2bc
Rahul kumar <rahul850kr@gmail.com>
2022-09-28 15:33:17 +0530

    auth backend

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+
+const {userController} = require("./Routes/user.routes");
+const {connection} = require("./configs/db")
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("Homepage")
+});
+
+app.use("/user",userController)
+
+app.listen(PORT,async ()=>{
+    try{
+        await connection;
+        console.log("Connected To DB Success")
+    }
+    catch(err){
+        console.log("Failed to connect to DB")
+    }
+    console.log(`Listening on port ${PORT}`)
+});
